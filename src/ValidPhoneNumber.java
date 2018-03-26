8be9d9e51f99e52054d24a3cf8bf54b73322e85a
Mike Baldwin <mikebaldwin21@gmail.com>
2018-03-26 15:45:18 -0400

    Fixed.

--- src/ValidPhoneNumber.java
+++ src/ValidPhoneNumber.java
@@ -1,25 +1,24 @@
 import javax.swing.*;
 
 public class ValidPhoneNumber {
+// Written by: Mike Baldwin
+// Project 6 - Part 1 - Valid Phone Numbers
+// This program reads in a string and checks whether it has the correct format.
+// The only format that has to be checked is "###-###-####"
+
 public static void main(String[] args) {
     String phoneNumber = JOptionPane.showInputDialog("Please enter a valid phone number:");
 
     if (isValid(phoneNumber))
-        JOptionPane.showMessageDialog(null, "Valid number. Thank you.");
+        JOptionPane.showMessageDialog(null, "Valid number.");
+    else
+        JOptionPane.showMessageDialog(null, "Invalid number.");
 }
 
 private static boolean isValid(String phNum) {
-    int count;
-
-    if (phNum.length() != 12)
-        return false;
-
-        count = 0;
-    for (char c : phNum.toCharArray()) {
-        count++;
-        if (Character.isDigit(c) || (c == '-' && (count == 4 || count == 8)))
+    for (int i = 0; i < phNum.length(); i++) {
+        if (Character.isDigit(phNum.charAt(i)) || (phNum.charAt(i) == '-' && (i == 3 || i == 7)))
             continue;
-        JOptionPane.showMessageDialog(null, "Invalid number.");
         return false;
     }
 
